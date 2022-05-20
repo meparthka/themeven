decd53b9f232068ac0e5472f926578cc4f3cb863
meparthka <parthka.2005@gmail.com>
2022-05-20 21:07:12 +0530

    add letest post div

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from 'next'
+import Link from 'next/link';
 import Header from '../components/header';
 import HomeHeader from '../components/homeheader';
 
@@ -7,8 +8,16 @@ const Home: NextPage = () => {
     <>
       <Header />
       <HomeHeader />
-      <h1>Hello, World!</h1>
-      <h1>Hello, World!</h1>
+      <div className="con letest-post">
+        <div className="featured-tag">
+          Latest Post
+        </div>
+        <h1 className="title-l">Using Deferred Static Generation with Analytics Tools</h1>
+        <p className="des-l">Only want to build out the most popular pages as static pages? No problem, you can use your analytics tool to control the usage of Deferred Static Generation in Gatsby.</p>
+        <div className="btn btn-success">
+          <Link href="/about">Continue Reading</Link>
+        </div>
+      </div>
       <h1>Hello, World!</h1>
       <h1>Hello, World!</h1>
       <h1>Hello, World!</h1>
