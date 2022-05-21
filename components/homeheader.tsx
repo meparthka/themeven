5bc04574bedd03539fe2221bd97e08f44e5c46f6
meparthka <parthka.2005@gmail.com>
2022-05-21 18:03:38 +0530

    add replecable word

--- components/homeheader.tsx
+++ components/homeheader.tsx
@@ -8,7 +8,7 @@ const HomeHeader: NextPage = () => (
       <HomeSvg />
       <div className="con">
         <h1 className="title-x">Hi, I Am Parthka!</h1>
-        <p className="des-l-alt">Welcome to my page.</p>
+        <p className="des-l-alt">Welcome to my website.</p>
         <p className="des-x">Backend Developer from <a className="des-x-link" rel="noreferrer" href="https://www.google.com/search?q=Gujarat%2C+India" target="_blank">Gujarat, India</a>.</p>
         <div className="btn btn-success">
           <Link href="/about">Contact Me</Link>
